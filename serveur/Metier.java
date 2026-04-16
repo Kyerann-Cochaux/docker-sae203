ee1d06d6d6bf98292f2f2499068e31dc37c3171d
BapaumeEthan <ethan.bapaume@etu.univ-lehavre.fr>
2026-04-16 17:59:25 +0200

    Création des méthodes de base

--- serveur/Metier.java
+++ serveur/Metier.java
@@ -6,11 +6,67 @@ public class Metier
 
 	public metier(Serveur srv)
 	{
-		this.initMorpion();
+		this.srv = srv;
+
+		this.plateau = new char[][] { { '0', '0', '0'},
+		                              { '0', '0', '0'},
+		                              { '0', '0', '0'} };
 	}
 	
-	public void initMorpion()
+	/*------------------------------------------------------*/
+	/* Méthodes pour traiter les commandes                  */
+	/*------------------------------------------------------*/
+
+	public void connexion(int numClient)
 	{
 		
+	}
+
+	public void resynchronise(int numClient)
+	{
+
+	}
+
+	public void etatMorpion(int numClient, String etat)
+	{
+
+	}
+
+	public void dessiner(int numClient, int lig, int col)
+	{
+
+	}
+
+	//Méthode qui va reconnaître les commandes
+	public boolean traiterCommande(String cmd)
+	{
+		int numClient = Integer.parseInt("" + cmd.charAt(3));
+
+		String ordre = "";
+
+		for(int cpt = 5; cpt < cmd.length(); cpt++)
+			ordre += "" + cmd.charAt(cpt);
+
+		switch (ordre)
+		{
+			case ".":
+				{
+					
+					break;
+				}
+		
+			case "..":
+				{
+					
+					break;
+				}
+				
+			case "morpi_etatmorpion":
+				{
+					String etat = cmd.substring(23, cmd.length());
+					break;
+				}
+		}
+		return true;
 	}
 }
