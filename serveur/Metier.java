eeadbe510f3c1fb0e5508959457b057b5eb4072c
BapaumeEthan <ethan.bapaume@etu.univ-lehavre.fr>
2026-04-16 16:52:03 +0200

    Ajout de la base du métier serveur

---
+++ serveur/Metier.java
@@ -0,0 +1,16 @@
+public class Metier
+{
+	private char[][] plateau;
+
+	private Serveur srv;
+
+	public metier(Serveur srv)
+	{
+		this.initMorpion();
+	}
+	
+	public void initMorpion()
+	{
+		
+	}
+}
