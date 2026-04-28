21c33e859584258c9f2d32c40e25cf79189affdb
Maël Couasnon <mael.couasnon@etu.univ-lehavre.fr>
2026-04-28 13:55:35 +0200

    Commencement création client

--- client/Client.java
+++ client/Client.java
@@ -16,27 +16,70 @@ public class Client
 {
 	// Attributs
 	private Controleur ctrl ;
+	private int        numJoueur ;
 	
 	// Constructeurs
-	private Client ( Controleur ctrl )
+	public Client()
 	{
-		this.ctrl = ctrl ;
+		this.ctrl      = new Controleur(this) ;
+		this.numJoueur = -1 ;
 	}
+	
+	// Accesseur
+	public int getNumJoueur() { return this.numJoueur ; }
+	
+	// Modifieur
+	public void setNumJoueur( int nvNum ) { this.numJoueur = nvNum ; }
 	
 	// Autres Méthodes
-	private void envoyerCmd( String cmd )
+	public String envoyerCmd( String cmd )
 	{
 		
+		
+		return null ;
 	}
 	
-	private void recevoirCmd( String cmd )
+	public void recevoirCmd( String cmd )
 	{
-		
+		this.ctrl.traiterCmd(cmd);
 	}
 	
 	// Méthode Principale
-	public static void main(String[] args)
+	public static void main(String[] arg)
 	{
-		
+		try
+		{
+			// Vérifications du Nom et Port de l'Hôte attendu en Arguments
+			String nomHote  = "" ;
+			String portHote = "" ;
+			
+			if ( arg.length == 2 )
+			{
+				nomHote  = arg[0];
+				portHote = arg[1];
+			}
+			else
+			{
+				System.out.println("Utilisation : ClientSimple2 [NomHôte] [PortHôte]");
+				System.exit(1);
+			}
+			// Connexion
+			Socket cli = new Socket( nomHote, Integer.parseInt(portHote) );
+			// Création de l'obj Client
+			Client clt = new Client();
+			
+			// Ouverture du de l'input
+			BufferedReader in = new BufferedReader( new InputStreamReader(cli.getInputStream()) );
+			
+			// Ouverture de l'Envoie de Texte
+			PrintWriter out = new PrintWriter( cli.getOutputStream(), true );
+			
+			
+			
+		}
+		catch ( IOException e )
+		{
+			e.printStackTrace();
+		}
 	}
 }
