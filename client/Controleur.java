a049ef05f9ace07836b327f41b11cd43b5fc475f
Cochaux Kyerann <kyerann.cochaux@etu.univ-lehavre.fr>
2026-04-28 14:48:46 +0200

    Implementaion of the method traiterCmd

--- client/Controleur.java
+++ client/Controleur.java
@@ -8,9 +8,11 @@ public class Controleur
 	
 	private String testStringMorpionCetteVariableVaEtreSupprime;
 	
-	public Controleur()
+	public Controleur(Client clt)
 	{
 		this.testStringMorpionCetteVariableVaEtreSupprime = "000000000";
+		
+		this.client = clt;
 		
 		this.metier = new Metier(this);
 		this.ihm    = new FrameGrille ( this );
@@ -31,19 +33,78 @@ public class Controleur
 		this.ihm.majIHM();
 	}
 	
-	public static void main(String[] a)
+	public void traiterCmd(String cmd)
 	{
-		new Controleur();
+		if ( this.client.getNumJoueur() == -1 )
+		{
+			switch (cmd)
+			{
+				case "srv_systm_attentejoueur_1":
+					this.client.setNumJoueur(1);
+					this.client.envoyerCmd("clt1_systm_connexion" );
+					break;
+				
+				
+				case "srv_systm_attentejoueur_2":
+					this.client.setNumJoueur(1);
+					this.client.envoyerCmd("clt2_systm_connexion" );
+					break;
+				
+				
+			}
+		}
+		
+		// TODO "srv_morpi_updatemorpion_XXXXXXXXX"
+		
+		if ( this.client.getNumJoueur() == -1 ) return;
+		
+		
+		switch (cmd)
+		{
+			
+			case "srv_morpi_initmorpion":
+				this.testStringMorpionCetteVariableVaEtreSupprime = "000000000";
+				this.client.envoyerCmd("clt" + this.client.getNumJoueur() + "_morpi_initialise");
+				break;
+			
+			
+			case "srv_morpi_checketatmorpion":
+				this.client.envoyerCmd("clt" + this.client.getNumJoueur() + "_morpi_etatmorpion_" + this.metier.toString() );
+				break;
+			
+			
+			case "srv_morpi_attactionj1":
+				
+				break;
+			
+			
+			case "srv_morpi_attactionj2":
+				
+				break;
+			
+			
+			case "srv_morpi_j1gagner":
+				// je ne sait pas
+				if ( this.client.getNumJoueur() == 1 ) System.out.println("T'as gagné :)" ); //on gagne
+				else                                   System.out.println("T'as perdu :(" ); //on perd
+				break;
+			
+			
+			case "srv_morpi_j2gagner":
+				// je ne sait pas
+				if ( this.client.getNumJoueur() == 2 ) System.out.println("T'as gagné :)" ); //on gagne
+				else                                   System.out.println("T'as perdu :(" ); //on perd
+				break;
+		
+			default:
+				break;
+		}
 	}
-
-	public void dessiner(int lig, int col) 
+	
+	public void dessiner(int lig, int col)
 	{
 		// méthode dans Client.java
-		// this.client.dessiner(lig, col);
-		
-		// Pour test : 
-		int let = col + lig * this.metier.getNbCol();
-		this.testStringMorpionCetteVariableVaEtreSupprime = this.testStringMorpionCetteVariableVaEtreSupprime.substring(0,let) + 'x'+ this.testStringMorpionCetteVariableVaEtreSupprime.substring(let+1);
+		this.client.envoyerCmd("clt" + client.getNumJoueur() + "_morpi_dessiner_" + lig + "" + col);
 		
 		this.metier.updateMorpion();
 		
